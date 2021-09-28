74d42345b946ec5cab97ea33fcbe8b5c2a9a96f1
José Southerland Silva <82189997+jsoutherlands@users.noreply.github.com>
2021-09-28 11:27:31 -0300

    Update buscador.c

--- Tarea-1/buscador.c
+++ Tarea-1/buscador.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 FILE* archivo;
 char bufferStart[50] = "Laberinto/";
 char* i1, i2, i3;
-for (int i = 1; i < 4; i++)
+char frase1[50], frase2[50], frase3[50], fraseAux[50];
+int* i, j, k;
+for (i = 1; i < 4; i++)
 {
-	for (int j = 1; j < 4; i++)
+	for (j = 1; j < 4; j++)
 	{
-		for (int k = 1; k < 4; i++)
+		for (k = 1; k < 4; k++)
 		{
 			asprintf(&i, "%d", i1);
 			asprintf(&j, "%d", i2);
 			asprintf(&k, "%d", i3);
 			strcat(strcat(strcat(strcat(strcat(bufferStart, i1), "/"), i2), "/"), i3);
+			if (archivo = fopen("frase.txt", "r"))
+			{
+				fscanf(archivo, "%s", fraseAux);
+				if (fraseAux[0] == "1")
+				{
+					printf("Funciona\n");
+				}
+			}
 		}	
 	}
 }
