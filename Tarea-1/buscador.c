4ab2ccee855e292d131f00b7f5c5f9342e278781
CataSierraH <90270004+CataSierraH@users.noreply.github.com>
2021-09-28 15:04:08 -0300

    Update buscador.c

--- Tarea-1/buscador.c
+++ Tarea-1/buscador.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-FILE* archivo;
-char bufferStart[50] = "Laberinto/";
-char* i1, i2, i3;
-char frase1[50], frase2[50], frase3[50], fraseAux[50];
-int* i, j, k;
-for (i = 1; i < 4; i++)
-{
-	for (j = 1; j < 4; j++)
-	{
-		for (k = 1; k < 4; k++)
+int main(){
+	FILE* archivo;
+	char bufferStart[50] = "Laberinto/";
+	char * i1;
+	char* i2;
+	char * i3;
+	char frase1[50], frase2[50], frase3[50], fraseAux[50];
+	int  i =1, j=1, k=1;
+	for (int m = 1; m < 4; m++)
+	{	
+		asprintf(&i1, "%d", m);
+		
+		for (int n = 1; n < 4; n++)
 		{
-			asprintf(&i, "%d", i1);
-			asprintf(&j, "%d", i2);
-			asprintf(&k, "%d", i3);
-			strcat(strcat(strcat(strcat(strcat(bufferStart, i1), "/"), i2), "/"), i3);
-			if (archivo = fopen("frase.txt", "r"))
+			asprintf(&i2, "%d", n);
+		
+			for (int o = 1; o < 4; o++)
 			{
-				fscanf(archivo, "%s", fraseAux);
-				if (fraseAux[0] == "1")
+				
+				asprintf(&i3, "%d", o);
+				strcat(strcat(strcat(strcat(strcat(bufferStart, i1), "/"), i2), "/"), i3);
+				strcat(bufferStart,"/frase.txt");
+				printf("%s\n",bufferStart);
+
+				if (archivo = fopen("frase.txt", "r"))
 				{
-					printf("Funciona\n");
+					fscanf(archivo, "%s", fraseAux);
+					printf("Frase aux %s\n",fraseAux);
+					printf("Frase aux[0]%d\n",fraseAux[0] );
+					//if (fraseAux[0] == "1")
+					//{
+					//	printf("Funciona\n");
+					//}
+					fclose(archivo);
 				}
-			}
-		}	
+				strcpy(bufferStart,"Laberinto/");
+			}	
+		
+		}
+	
 	}
+	return 0;
 }
