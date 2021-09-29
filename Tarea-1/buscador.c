01e1f5772b73422e471cff858d5d23ab8825bc51
José Southerland Silva <82189997+jsoutherlands@users.noreply.github.com>
2021-09-29 02:17:09 -0300

    Update buscador.c

--- Tarea-1/buscador.c
+++ Tarea-1/buscador.c
@@ -1,34 +1,35 @@
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE 1
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 int main(){
 	FILE* archivo;
 	char bufferStart[50] = "Laberinto/";
-	char * i1;
+	char* i1;
 	char* i2;
-	char * i3;
+	char* i3;
 	char frase1[50], frase2[50], frase3[50], numerito[1], inutil[3], fraseFinal[75], auxiliar[25];
-	int  i =1, j=1, k=1;
-	for (int m = 1; m < 4; m++)
+	for (int i = 1; i < 4; i++)
 	{	
-		asprintf(&i1, "%d", m);
+		asprintf(&i1, "%d", i);
 		
-		for (int n = 1; n < 4; n++)
+		for (int j = 1; j < 4; j++)
 		{
-			asprintf(&i2, "%d", n);
+			asprintf(&i2, "%d", j);
 		
-			for (int o = 1; o < 4; o++)
+			for (int k = 1; k < 4; k++)
 			{
 				
-				asprintf(&i3, "%d", o);
+				asprintf(&i3, "%d", k);
 				strcat(strcat(strcat(strcat(strcat(bufferStart, i1), "/"), i2), "/"), i3);
 				strcat(bufferStart,"/frase.txt");
 				printf("%s\n",bufferStart);
 
-				if (archivo = fopen(bufferStart, "r"))
+				if ((archivo = fopen(bufferStart, "r")) != NULL)
 				{
 					fscanf(archivo, "%c", numerito);
-					
 					fscanf(archivo, "%s", inutil);
 					
 					while(!feof(archivo)){
