6beba6d0b2f75febd97b89fd11cbc963260498cc
CataSierraH <90270004+CataSierraH@users.noreply.github.com>
2021-10-28 16:12:32 -0300

    Update tablero.c

--- Tarea-2/tablero.c
+++ Tarea-2/tablero.c
@@ -12,6 +12,26 @@
 
 #define NAME_SHMEM "/tablero1"
 
+
+/***********************
+* 
+* Tablero *crearTablero
+* 
+************************
+* 
+* Return: Tablero
+* 
+************************
+* 
+* Descripción: Se crea e inicializa el tablero
+* de Monopoly. Se crea y asigna memoria compartida,
+* la cual será utilizada por todos los procesos. 
+* Además, se asigna la posición inicial de todos
+* los jugadores (en START) y la repartición inicial
+* de dinero ($100).
+* 
+************************/
+
 Tablero *crearTablero(){
 	int fd;
 	Tablero *newTablero;
@@ -20,9 +40,6 @@ Tablero *crearTablero(){
 	ftruncate(fd,sizeof(Tablero)); //asigna el espacio a la memoria compartida
 	newTablero= mmap(0, sizeof(Tablero), PROT_WRITE, MAP_SHARED, fd, 0);
 	newTablero->len = 28;
-	printf("len = %d\n", newTablero->len);
-	//printf("Tablero len: %d\n", newTablero->len);
-
 	for (int i = 0; i < 27; i++){
 		newTablero->casillas[i].posicion = i+1;
 		for (int j = 0; j < 3; j++){
@@ -30,7 +47,6 @@ Tablero *crearTablero(){
 			newTablero->casillas[i].jugadores[j] = 0;
 		}
 	}
-
 	newTablero->casillas[0].dato = 100;
 	newTablero->casillas[1].dato = 50;
 	newTablero->casillas[2].dato = 50;
@@ -60,15 +76,33 @@ Tablero *crearTablero(){
 	newTablero->casillas[26].dato = -25;
 	newTablero->casillas[27].dato = -75;
 
-
-	//A considerar: ver tail y head!!!
-
 	for (int k = 0; k < 3; k++){
 		newTablero->dinero[k] = 100;
 		newTablero->casillas[0].jugadores[k] = 1;
 	}
 	return newTablero;
 }
+
+/***********************
+* 
+* int getJugador
+* 
+************************
+* 
+* Parámetros:
+* Tablero* 	tablero
+* int 		jugador (número de jugador).
+* 
+************************
+* 
+* Return: i (posición del jugador en el tablero).
+* 
+************************
+* 
+* Descripción: Se busca casilla por casilla
+* la posición del jugador buscado.
+* 
+************************/
 
 int getJugador(Tablero* tablero, int jugador){
 	for (int i = 0; i < tablero->len; i++){
@@ -79,15 +113,35 @@ int getJugador(Tablero* tablero, int jugador){
 	return 0;
 }
 
+/***********************
+* 
+* void moveToPos
+* 
+************************
+* 
+* Parámetros:
+* Tablero* 	tablero
+* int 		dado (cantidad de casillas (+ o -) a mover).
+* int 		jugador (número de jugador).
+* 
+************************
+* 
+* Descripción: Mueve la posición del jugador a la casilla
+* correspondiente, de acuerdo al número indicado en la variable
+* dado. Cuando el jugador da la vuelta al tablero, se le adicionan $100
+* a su dinero, y se reinicia la posición a 0 o más, según sea necesario.
+* 
+************************/
+
 void moveToPos(Tablero* tablero, int dado, int jugador){
 	int posicion = getJugador(tablero, jugador);
 	int nuevaposicion = posicion + dado;
-	if (nuevaposicion >= 27){
-		nuevaposicion = nuevaposicion-27;
+	if (nuevaposicion > 27){
+		nuevaposicion = nuevaposicion-28;
 		tablero->dinero[jugador] += 100;
+		printf("Has pasado por START. Ganas +$100\n");
 	}
 	tablero->casillas[posicion].jugadores[jugador] = 0;
 	tablero->casillas[nuevaposicion].jugadores[jugador] = 1;
 };
-
 
