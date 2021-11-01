741b7f423e095d86cd915ebe024783846d252f1a
José Southerland Silva <82189997+jsoutherlands@users.noreply.github.com>
2021-11-01 18:11:15 -0300

    Update tablero.h

--- Tarea-2/tablero.h
+++ Tarea-2/tablero.h
@@ -1,18 +1,23 @@
 #ifndef _TABLERO_H
 #define _TABLERO_H
 
+//Struct de "casilla" de tablero.
 typedef struct _nodo{
 	int posicion;
 	int dato;
 	int jugadores[3];
 } tipoNodo;
 
+//Struct de tablero.
 typedef struct tablero{
-	tipoNodo casillas[27];
+	tipoNodo casillas[28];
 	int dinero[3];
+	int pids[3];
 	int len;
 } Tablero;
 
+char mostrarPosicion(Tablero* tablero, int casilla, int jugador);
+void mostrarTablero(Tablero* tablero);
 Tablero *crearTablero();
 int getJugador(Tablero* tablero, int jugador);
 void moveToPos(Tablero* tablero, int dado, int jugador);
